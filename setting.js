7c18e5b04a2a8064186c2e8b2535f56592342750
lasta6310-blip <lasta6310@gmail.com>
2026-02-27 17:51:55 +0500

    Update setting.js

--- setting.js
+++ setting.js
@@ -7,7 +7,7 @@ module.exports = {
   PREFIXES: ['.', '!', '#'], // Multiple prefix support you can add one or more
   
   
-  SESSION_ID: '', // add your session ID
-  PAIRING_NUMBER: '', // In Case if you don't have session id get pairing code by putting your number here
+  SESSION_ID: 'KnightBot!H4sIAAAAAAAAA5VU0ZKiOBT9l7xqjYAISlVXDSKNoCgI0ujWPEQIGEXAJIA45b9v0T3dPQ+7s71v4ZI699xzzs1PkBeYogVqgfITlATXkKHuyNoSAQVMqyRBBPRBDBkECrCdttWjwNvo/H6524XqYS8g6SbftfjZ5CVDXK8j9Q7p7cw9gUcflNUhw9EfAM2R82I/v0zDnR7YlbXDpjFpJpxRWtBLng96eIKnijUXeqRP4NEhQkxwnurlEV0QgdkCtQ7E5Gv0rzOjZ0F1kq3lZe2yU+2I/snoTTaSmMGbz19HNZOt1jsF9tfo67UUJL2K+slLJe2C2TWBzvSsrzAbxFOjF8D9dLC4LvmN0LzRpzjNUWzGKGeYtV/W3ZiKYbCp+cjSNTxLcLy22Jj2VI9QD5fmVbg7KL1v85ch/Rpx/Dy0XoSxsR5PLsgfseWWXFxO5iR+MPW9wC0teZ/m65bed78Td8h7Vs7/R3dqLqrU1fN9s6m2kVzf6rPVzNxdUotG7t2Okx3zArbabvnoa/RX3sbPY3K6eL64P3tnH9syyaOWyNl0cSi1lRvn1qElo6b4pA9ZRf7E8uKnc1GXZcndQVQXquao863ghcW8PsZhqKbLC9obttOofprNMxjitUec63kZ33cZFPIXfe2acW+dixJn2ol5OiSq5j69TnRGrRkDhX/0AUEppoxAhou8qwmi3Acwrj0UEcRe5QWJvHKMwF2IV+YFxBbh3a/unH/irFFvJfj3tMijcO+U62HzBPqgJEWEKEXxHFNWkNZGlMIUUaD89aMPcnRjb8Z17YZ8HySYULbNqzIrYPzu6vtPGEVFlTOvzSOtOyACFO6zjBjDeUo7HasckuiIa6QdIaNASWBG0ceEiKAYKIxU6GNrtSLuhHd3gWjwwQb0weXVEBwDBUyE4VAeDfnJSJCU4Xf6relQYVl+yxEDfZC93uJlWZJFmefE0VgcDZXh967++ODXwcWIQZxRoADNQuJkIs51By+PlDMMVU9VLVXB5zzvwXgTXtgmXugluyIa9Q6cJj/X5NZk54HqVm69HJjtUF9MTXscuvrTP4AABaythhY4PniX5apXM8PRq1Ay1NWol3hNs7/bvWQmc47kWb7KMLcZGGwGzXETpX5Qvex3AbHn22Gtze43x74PPGejldMuRX0QoxpH6PdmmT9298ObtFmQPC3xSaqmUV7INjMCfmsIi9g1SlnOpG058BxnP+Li60A48JF6PKtOMAq5WNvgsFmw3vK+xmEdTA6Nmr5F9nVlsl9PFX4NU+dU95lg9Lr5Oez8+0/n3nh3+eIe/d8gfj0l/7KO0zibcxmXC4v5OrlM1Op2kMaRtXcafXletoPWZjct9WbOMzcFj8ePPigzyJKCXIACYB6TAsegD0hRdYE186T4QzNN5cypm2rd4BmkTP1cAh9fEGXwUgKFl2WBnwwFWXq75ZCinEN67HK5EI9zETz+Bh1UmBVSBwAA', // add your session ID
+  PAIRING_NUMBER: '923375319526', // In Case if you don't have session id get pairing code by putting your number here
   TIMEZONE: 'Africa/Harare' // ur timezone
 };
